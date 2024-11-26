e84fd74f28b44412ed4ed465ca1b74b903b3a4db
Rivenf <39199060+rivenf@users.noreply.github.com>
2024-11-26 18:05:34 +0000

    Build Gallery component

--- design-showcase/src/Gallery.js
+++ design-showcase/src/Gallery.js
@@ -1,7 +1,48 @@
+import photoData from "./photoData.json";
+import { useState } from "react";
+
+function Photo({ photo }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleOverlay() {
+    isOpen ? setIsOpen(false) : setIsOpen(true);
+  }
+
+  return (
+    <>
+      <div className="galleryImg">
+        <img src={photo.thumbnail} alt={photo.title} onClick={toggleOverlay} />
+      </div>
+
+      {isOpen ? (
+        <div className="overlay">
+          <div className="overlayContainer">
+            <button className="galleryBtn" onClick={toggleOverlay}>
+              X
+            </button>
+            {/* <img
+              src={photo.thumbnail}
+              alt={photo.title}
+              className="overlayImg"
+            /> */}
+            <p>Pictures go here - to do later</p>
+            <h3>{photo.title}</h3>
+            <p>{photo.description}</p>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+}
+
 export default function Gallery() {
+  const photos = photoData.photos;
+
   return (
     <div className="gallery">
-      <p>Pictures go here</p>
+      {photos.map((photo) => (
+        <Photo photo={photo} key={photo.id} />
+      ))}
     </div>
   );
 }
